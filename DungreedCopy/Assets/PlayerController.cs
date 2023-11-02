6f0c3e03dbd398d643001f4b1c925d9cc1f93bb5
Seoptank <cnpdi1118@naver.com>
2023-11-02 20:58:35 +0900

    PlayerJump
    While implementing the jump function, I also implemented the long jump and double jump functions.

--- DungreedCopy/Assets/PlayerController.cs
+++ DungreedCopy/Assets/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private KeyCode         jumpKey = KeyCode.Space;
     private Movement2D      movement;
     private void Awake()
     {
@@ -13,6 +15,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         UpdateMove();
+        UpdateJump();
     }
 
     public void UpdateMove()
@@ -20,5 +23,25 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
 
         movement.MoveTo(x);
+    }
+    public void UpdateJump()
+    {
+        if(Input.GetKeyDown(jumpKey))
+        {
+            bool isJump = movement.JumpTo();
+
+            if(isJump == true)
+            {
+                // 점프 성공시 To Do
+            }
+        }
+        else if (Input.GetKey(jumpKey))
+        {
+            movement.isLongJump = true;
+        }
+        else if (Input.GetKeyUp(jumpKey))
+        {
+            movement.isLongJump = false;
+        }
     }
 }
